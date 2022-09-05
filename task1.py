af129ca6dca1ab81a1d9e539860eed95c82ac1f5
Prasanna Venkatesan <83625774+Prasanna-Venkatesan-6997@users.noreply.github.com>
2022-09-05 13:43:33 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,14 +1,16 @@
-!pip install apache_beam
+#!pip install apache_beam
+
+#I used the above command to install apache_beam 
 
 import apache_beam as beam
 
-with beam.Pipeline() as pipe:
+with beam.Pipeline() as pipe:         #Naming the pipeline as pipe
   ip = (pipe
-      |beam.io.ReadFromText("/content/transactions.csv", skip_header_lines=True)
-      |beam.Map(lambda x:x.split(","))  
-      |beam.Filter(lambda x:x[1]>"20.00")
-      |beam.Filter(lambda x:x[0]>"2009-12-31")
-      |beam.Map(lambda x:(x[0], float(x[1])))
-      |beam.CombinePerKey(sum)
-      |beam.io.WriteToText("/content/transactions.csv", header=["date","total_amount"])
+      |"Reading the file from gs" >> beam.io.ReadFromText("gs://cloud-samples-data/bigquery/sample-transactions/transactions.csv", skip_header_lines=True)
+      |"Splitting the columns" >> beam.Map(lambda x:x.split(","))  
+      |"filtering the transaction_amount greater than 20" >> beam.Filter(lambda x:x[1]>"20.00")
+      |"filtering out date before 2010" >> beam.Filter(lambda x:x[0]>"2009-12-31")
+      |"changling the datatype of transaction_amount to float" >> beam.Map(lambda x:(x[0], float(x[1])))
+      |"summing the transaction_amount by date" >> beam.CombinePerKey(sum)
+      |"writing output to csv file with the columns date and total_amount" >> beam.io.WriteToText("/output/results.csv", header=["date","total_amount"])
 )
